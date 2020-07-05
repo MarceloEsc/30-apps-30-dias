e9541148d3dabcaa7e03f30504416232eafc735d
MarceloEsc <marceloescobarjunior@gmail.com>
2020-07-05 00:26:55 -0300

    pwa concluido

--- markdown previewer/worker.js
+++ markdown previewer/worker.js
@@ -21,11 +21,17 @@ function precache() {
             './style.css',
             './github.markdown.css',
             './manifest.json',
+            './images/icon72.png',
+            './images/icon144.png',
             './app.js',
             './FileSaver.js',
             './worker.js',
             'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css',
             'https://cdn.jsdelivr.net/npm/marked/marked.min.js',
+            'https://fonts.googleapis.com/css2?family=Anton&family=Roboto+Mono:wght@500&display=swap',
+            'https://fonts.gstatic.com/s/anton/v11/1Ptgg87LROyAm3Kz-C8.woff2',
+            'https://fonts.gstatic.com/s/robotomono/v11/L0xuDF4xlVMF-BfR8bXMIhJHg45mwgGEFl0_7Pq_ROW4.woff2',
+            'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/webfonts/fa-regular-400.woff2',
         ]);
     });
 }
